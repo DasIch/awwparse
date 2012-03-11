1359a8bc300a2239749ad36e0fd563f828ff452b
Daniel Neuhäuser <dasdasich@gmail.com>
2012-03-11 03:42:46 +0100

    Let make_command return an instance

--- awwparse/testsuite/init.py
+++ awwparse/testsuite/init.py
@@ -41,17 +41,17 @@ def make_command(options=None, commands=None, command_cls=Command):
         "options": {} if options is None else options,
         "commands": {} if commands is None else commands,
         "main": lambda self, **kwargs: kwargs
-    })
+    })()
 
 
 class OptionTestCase(TestCase):
     def test_signature(self):
-        command = make_command({"option": Option("o", Bytes(), Bytes(), Bytes())})()
+        command = make_command({"option": Option("o", Bytes(), Bytes(), Bytes())})
         for args in [["-o"], ["-o", "foo"], ["-o", "foo", "bar"]]:
             with self.assert_raises(ArgumentMissing):
                 command.run(args)
 
-        command = make_command({"option": Option("o", Bytes(), [Bytes(), Bytes()])})()
+        command = make_command({"option": Option("o", Bytes(), [Bytes(), Bytes()])})
         self.assert_(command.options["option"].types[1].optional)
         self.assert_equal(command.run(["-o", "a"]), {"option": ["a"]})
         self.assert_equal(
@@ -64,7 +64,7 @@ class OptionTestCase(TestCase):
 
         command = make_command({
             "option": Option("o", Bytes(), [Bytes(), [Bytes()]])
-        })()
+        })
         self.assert_(
             all(type.optional for type in command.options["option"].types[1:])
         )
@@ -76,49 +76,49 @@ class OptionTestCase(TestCase):
             )
 
     def test_default(self):
-        command = make_command({"option": Option("o", Bytes())})()
+        command = make_command({"option": Option("o", Bytes())})
         self.assert_equal(command.options["option"].default, missing)
         self.assert_equal(command.run([]), {})
 
         command = make_command(
             {"option": Option("o", Bytes(default="foobar"))}
-        )()
+        )
         self.assert_equal(command.options["option"].default, "foobar")
         self.assert_equal(command.run([]), {"option": "foobar"})
 
     def test_abbreviation_prefix(self):
-        command = make_command({"option": Option("o", Bytes())})()
+        command = make_command({"option": Option("o", Bytes())})
         self.assert_equal(command.options["option"].abbreviation_prefix, "-")
         self.assert_(command.options["option"].matches("-o"))
 
         command = make_command({
             "option": Option("o", Bytes(), abbreviation_prefix="+")
-        })()
+        })
         self.assert_equal(command.options["option"].abbreviation_prefix, "+")
         self.assert_(command.options["option"].matches("+o"))
         self.assert_equal(command.run(["+o", "foo"]), {"option": "foo"})
 
     def test_name_prefix(self):
-        command = make_command({"option": Option("option", Bytes())})()
+        command = make_command({"option": Option("option", Bytes())})
         self.assert_equal(command.options["option"].name_prefix, "--")
         self.assert_(command.options["option"].matches("--option"))
 
         command = make_command({
             "option": Option("option", Bytes(), name_prefix="++")
-        })()
+        })
         self.assert_equal(command.options["option"].name_prefix, "++")
         self.assert_(command.options["option"].matches("++option"))
         self.assert_equal(command.run(["++option", "foo"]), {"option": "foo"})
 
     def test_actions(self):
-        command = make_command({"option": Option("o", Bytes())})()
+        command = make_command({"option": Option("o", Bytes())})
         self.assert_equal(command.run(["-o", "foobar"]), {"option": "foobar"})
         self.assert_equal(
             command.run(["-o", "foo", "-o", "bar"]),
             {"option": "bar"}
         )
 
-        command = make_command({"option": Option("o", Bytes(), action=append)})()
+        command = make_command({"option": Option("o", Bytes(), action=append)})
         self.assert_equal(command.run(["-o", "foobar"]), {"option": ["foobar"]})
         self.assert_equal(
             command.run(["-o", "foo", "-o", "bar"]),
@@ -202,7 +202,7 @@ class ActionTestCase(TestCase):
             "a": Option("a", Bytes()),
             "b": Option("b", Bytes()),
             "c": Option("c", Bytes())
-        })()
+        })
         self.assert_equal(
             command.run(["-abc", "foo", "bar", "baz"]),
             {"a": "foo", "b": "bar", "c": "baz"}
